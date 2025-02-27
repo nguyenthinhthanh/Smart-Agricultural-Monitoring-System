25d0cdeff4b03ba592493661057a46a1168523a2
MSI\ADMINS <thanh.nguyenthinh@hcmut.edu.vn>
2025-02-27 20:19:34 +0700

    Implement time schedule watering

--- Microbit_Yolobit/SAMS.py
+++ Microbit_Yolobit/SAMS.py
@@ -4,22 +4,42 @@ button_a.on_pressed = None
 button_b.on_pressed = None
 button_a.on_pressed_ab = button_b.on_pressed_ab = -1
 from aiot_lcd1602 import LCD1602
-from event_manager import *
+from machine import RTC
+import ntptime
 import time
+from event_manager import *
 from machine import Pin, SoftI2C
 from aiot_dht20 import DHT20
 
+def on_mqtt_message_receive_callback__bbc_time_start_(server_time_start):
+  global server_mode, server_manual_watering, server_manual_temperature, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
+  display.scroll(server_time_start)
+  display.scroll((int((server_time_start[3 : 5]))))
+  time_start = (int((server_time_start[ : 2]))) * 60
+  time_start = (time_start if isinstance(time_start, (int, float)) else 0) + (int((server_time_start[3 : 5])))
+
+def on_mqtt_message_receive_callback__bbc_time_end_(server_time_end):
+  global server_mode, server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold
+  time_end = (int((server_time_end[ : 2]))) * 60
+  time_end = (time_end if isinstance(time_end, (int, float)) else 0) + (int((server_time_end[3 : 5])))
+
+# Mô tả hàm này...
+def schedule_watering():
+  global server_time_start, server_mode, server_manual_watering, server_manual_temperature, server_time_end, frame_mode, temperatue, time2, time_end, temperature_threshold, humidity, time_start, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
+  mqtt.on_receive_message('bbc-time-start', on_mqtt_message_receive_callback__bbc_time_start_)
+  mqtt.on_receive_message('bbc-time-end', on_mqtt_message_receive_callback__bbc_time_end_)
+
 def on_mqtt_message_receive_callback__bbc_server_mode_(server_mode):
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, server_manual_watering, frame_mode, server_manual_temperature
+  global server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
   frame_mode = int(server_mode)
 
 # Mô tả hàm này...
 def mode_selection():
-  global server_manual_watering, server_mode, server_manual_temperature, temperatue, frame_mode, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
+  global server_time_start, server_mode, server_manual_watering, server_manual_temperature, server_time_end, frame_mode, temperatue, time2, time_end, temperature_threshold, humidity, time_start, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
   mqtt.on_receive_message('bbc-server-mode', on_mqtt_message_receive_callback__bbc_server_mode_)
 
 def on_mqtt_message_receive_callback__bbc_manual_watering_(server_manual_watering):
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, frame_mode, server_mode, server_manual_temperature
+  global server_mode, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
   if frame_mode == 0:
     if server_manual_watering == '1':
       pin14.write_analog(round(translate(70, 0, 100, 0, 1023)))
@@ -28,11 +48,11 @@ def on_mqtt_message_receive_callback__bbc_manual_watering_(server_manual_waterin
 
 # Mô tả hàm này...
 def manual_watering():
-  global server_manual_watering, server_mode, server_manual_temperature, temperatue, frame_mode, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
+  global server_time_start, server_mode, server_manual_watering, server_manual_temperature, server_time_end, frame_mode, temperatue, time2, time_end, temperature_threshold, humidity, time_start, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
   mqtt.on_receive_message('bbc-manual-watering', on_mqtt_message_receive_callback__bbc_manual_watering_)
 
 def on_mqtt_message_receive_callback__bbc_manual_temperature_(server_manual_temperature):
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, server_manual_watering, frame_mode, server_mode
+  global server_mode, server_manual_watering, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
   if frame_mode == 0:
     if server_manual_temperature == '1':
       pin10.write_analog(round(translate(50, 0, 100, 0, 1023)))
@@ -41,17 +61,17 @@ def on_mqtt_message_receive_callback__bbc_manual_temperature_(server_manual_temp
 
 # Mô tả hàm này...
 def manual_temperature():
-  global server_manual_watering, server_mode, server_manual_temperature, temperatue, frame_mode, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
+  global server_time_start, server_mode, server_manual_watering, server_manual_temperature, server_time_end, frame_mode, temperatue, time2, time_end, temperature_threshold, humidity, time_start, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, aiot_lcd1602, aiot_dht20
   mqtt.on_receive_message('bbc-manual-temperature', on_mqtt_message_receive_callback__bbc_manual_temperature_)
 
 aiot_lcd1602 = LCD1602()
 
 def on_mqtt_message_receive_callback__bbc_temperature_threshold_(server_temp_threshold):
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_soil_threshold, server_manual_watering, frame_mode, server_mode, server_manual_temperature
+  global server_mode, server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_soil_threshold, server_time_end
   temperature_threshold = int(server_temp_threshold)
 
 def on_mqtt_message_receive_callback__bbc_soil_moisture_threshold_(server_soil_threshold):
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_manual_watering, frame_mode, server_mode, server_manual_temperature
+  global server_mode, server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_time_end
   soil_threshold = int(server_soil_threshold)
 
 event_manager.reset()
@@ -59,7 +79,7 @@ event_manager.reset()
 aiot_dht20 = DHT20()
 
 def on_event_timer_callback_Q_F_l_q_Y():
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, server_manual_watering, frame_mode, server_mode, server_manual_temperature
+  global server_mode, server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
   aiot_dht20.read_dht20()
   temperatue = aiot_dht20.dht20_temperature()
   humidity = aiot_dht20.dht20_humidity()
@@ -90,17 +110,37 @@ def on_event_timer_callback_Q_F_l_q_Y():
 event_manager.add_timer_event(10000, on_event_timer_callback_Q_F_l_q_Y)
 
 def on_event_condition_callback_R_d_t_i_X():
-  global temperatue, temperature_threshold, humidity, soil_threshold, light, soil_moisture, server_temp_threshold, server_soil_threshold, server_manual_watering, frame_mode, server_mode, server_manual_temperature
+  global server_mode, server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
   if (aiot_dht20.dht20_temperature()) > temperature_threshold:
     pin10.write_analog(round(translate(50, 0, 100, 0, 1023)))
+    mqtt.publish('bbc-manual-temperature', '1')
   else:
     pin10.write_analog(round(translate(0, 0, 100, 0, 1023)))
+    mqtt.publish('bbc-manual-temperature', '0')
   if (round(translate((pin0.read_analog()), 0, 4095, 0, 100))) < soil_threshold:
     pin14.write_analog(round(translate(70, 0, 100, 0, 1023)))
+    mqtt.publish('bbc-manual-watering', '1')
   else:
     pin14.write_analog(round(translate(0, 0, 100, 0, 1023)))
+    mqtt.publish('bbc-manual-watering', '0')
 
 event_manager.add_condition_event(lambda:(frame_mode == 1), on_event_condition_callback_R_d_t_i_X)
+
+def on_event_timer_callback_u_T_F_l_M():
+  global server_mode, server_manual_watering, server_manual_temperature, server_time_start, frame_mode, temperatue, time2, temperature_threshold, humidity, soil_threshold, light, time_start, soil_moisture, time_end, server_temp_threshold, server_soil_threshold, server_time_end
+  if frame_mode == 1:
+    time2 = (int(('%0*d' % (2, RTC().datetime()[4])))) * 60
+    time2 = (time2 if isinstance(time2, (int, float)) else 0) + (int(('%0*d' % (2, RTC().datetime()[5]))))
+    if time2 > time_start:
+      pin14.write_analog(round(translate(70, 0, 100, 0, 1023)))
+      mqtt.publish('bbc-manual-watering', '1')
+    if time2 > time_end:
+      pin14.write_analog(round(translate(0, 0, 100, 0, 1023)))
+      mqtt.publish('bbc-manual-watering', '0')
+  else:
+    display.scroll(frame_mode)
+
+event_manager.add_timer_event(5000, on_event_timer_callback_u_T_F_l_M)
 
 if True:
   aiot_lcd1602.clear()
@@ -109,20 +149,24 @@ if True:
   temperature_threshold = 30
   soil_threshold = 0
   frame_mode = 0
+  time_start = 1439
+  time_end = 1439
   display.scroll('SAMS')
   mqtt.connect_wifi('ACLAB', 'ACLAB2023')
   mqtt.connect_broker(server='io.adafruit.com', port=1883, username='nguyenthinhthanh', password='aio_ncvb01rNvHwY2qgnDZm9JBHzq6gE')
   display.scroll('Ok')
+  ntptime.settime()
+  (year, month, mday, week_of_year, hour, minute, second, milisecond) = RTC().datetime()
+  RTC().init((year, month, mday, week_of_year, hour+7, minute, second, milisecond))
   mqtt.on_receive_message('bbc-temperature-threshold', on_mqtt_message_receive_callback__bbc_temperature_threshold_)
   mqtt.on_receive_message('bbc-soil-moisture-threshold', on_mqtt_message_receive_callback__bbc_soil_moisture_threshold_)
   mode_selection()
   manual_watering()
   manual_temperature()
+  schedule_watering()
 
 while True:
   mqtt.check_message()
   event_manager.run()
-  display.scroll(temperature_threshold)
-  display.scroll(soil_threshold)
   time.sleep_ms(1000)
   time.sleep_ms(10)
